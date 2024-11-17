3fe4a29f021792265d7b30be962736638fd922f1
Bryan <107115383+bryanpg18@users.noreply.github.com>
2024-11-17 17:31:00 -0300

    javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,19 @@
-let count= 1;
-document.getElementById("radio1").checked=true;
-setInterval( function(){
-nextImage();
-}, 4000)
+let currentIndex = 0;
 
+function moveSlide(direction) {
+    const slides = document.querySelectorAll('.carrossel li');
+    const totalSlides = slides.length;
 
-function nextImage(){
-    count++;
-    if(count>4){
-        count=1;
+    currentIndex += direction;
+
+    // Verifica se o índice está fora dos limites
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
     }
-    document.getElementById(`radio${count}`).checked=true;
-   
+
+    // Move o carrossel
+    const carrossel = document.querySelector('.carrossel ul');
+    carrossel.style.transform = `translateX(-${currentIndex * 100}%)`; // Desloca as imagens
 }
