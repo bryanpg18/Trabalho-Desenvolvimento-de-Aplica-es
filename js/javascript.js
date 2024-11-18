d0b6ec2d8b279ae4ad2b24e95861713071dbabbc
GustavoRMenezes <gustavomenezes0099@gmail.com>
2024-11-18 03:22:48 -0300

    Ultima atualização 03:20

--- js/javascript.js
+++ js/javascript.js
@@ -1,19 +1,15 @@
-let currentIndex = 0;
+let count = 1;
+document.getElementById("radio1").checked = true;
 
-function moveSlide(direction) {
-    const slides = document.querySelectorAll('.carrossel li');
-    const totalSlides = slides.length;
+setInterval( function(){
+    nextImage();
+}, 4000)
 
-    currentIndex += direction;
-
-    // Verifica se o índice está fora dos limites
-    if (currentIndex < 0) {
-        currentIndex = totalSlides - 1;
-    } else if (currentIndex >= totalSlides) {
-        currentIndex = 0;
+function nextImage(){
+    count++;
+    if(count>5){
+        count = 1;
     }
 
-    // Move o carrossel
-    const carrossel = document.querySelector('.carrossel ul');
-    carrossel.style.transform = `translateX(-${currentIndex * 100}%)`; // Desloca as imagens
+    document.getElementById("radio"+count).checked = true;
 }
